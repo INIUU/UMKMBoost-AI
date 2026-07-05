9162c02390c7d83ad845dfff1e45436e42aebfe3
INI <158157872+INIUU@users.noreply.github.com>
2026-07-05 23:21:51 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,20 +7,15 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if (app()->environment('production')) {
-        URL::forceScheme('https');
+            URL::forceScheme('https');
+        }
     }
 }
